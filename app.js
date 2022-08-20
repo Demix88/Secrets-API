8ab5f5fba37127e479b123552daef2db0dd3d68a
Demix88 <skrasnauskis@gmail.com>
2022-08-20 22:07:24 +0100

    added cookie and new passport encryption

--- app.js
+++ app.js
@@ -1,82 +1,114 @@
 //jshint esversion:8
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-
-const app=express();
 const ejs = require("ejs");
-const bodyParser=require("body-parser");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const app = express();
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const mongoose = require("mongoose");
+app.set("trust proxy", 1); // trust first proxy
+app.use(
+  session({
+    secret: "This is my little secret I will never tell!",
+    resave: false,
+    saveUninitialized: false
+  }));
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+  
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+
 
 const userSchema = new mongoose.Schema({
-    email: String,
-    password:String
+  email: String,
+  password: String,
 });
 
-
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.get("/", (req, res) => {
-    res.render("home");
+  res.render("home");
 });
+
 app.get("/login", (req, res) => {
-    res.render("login");
+  res.render("login");
 });
+
 app.get("/register", (req, res) => {
-    res.render("register");
+  res.render("register");
 });
-app.post("/register", (req, res) => {
 
-    bcrypt.hash(req.body.password, saltRounds, (err, hash)=> {
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
-        newUser.save((err) => {
-            if (err) {
-                console.log(err);
-            } else {
-                res.render("secrets");
-            }
-        });
-        });
+
+app.get("/secrets", (req, res) => {
+    if (req.isAuthenticated()) {
+        res.render("secrets");
+    } else {
+        res.redirect("/login");
+    }
+});
+
+app.get("/logout", (req, res) => {
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+      });
     });
-
-
-
-app.post("/login", (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    User.findOne({ email: username }, (err, foundUser) => {
+  
+  
+app.post("/register", (req, res) => {
+    User.register({ username: req.body.username }, req.body.password, (err, user) => {
         if (err) {
             console.log(err);
-            
+            res.redirect("/re gister");
         } else {
-            if (foundUser) {
-                bcrypt.compare(password, foundUser.password, function (err, result) {
-                    if (result == true) {
-                        res.render("secrets");
- 
-                    }
-                });
-                    
-            }
+            passport.authenticate("local")(req, res, () => {
+                res.redirect("/secrets");
+            });
+            
+        }
+    });
+
+});
+
+app.post("/login", (req, res) => {
+
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    req.login(user, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            passport.authenticate("local")(req, res, () => {
+                res.redirect("/secrets");
+            });
         }
     });
 });
 
-let port=process.env.PORT ;
-if(port==null || port==""){
+let port = process.env.PORT;
+if (port == null || port == "") {
   port = 3000;
 }
 
-app.listen(port , ()=>{
-console.log("Server started succesfully "+port);
+app.listen(port, () => {
+  console.log("Server started succesfully " + port);
 });
