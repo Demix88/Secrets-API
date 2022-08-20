19862419b3cc67da7843649f51196a3e3eb32ca5
Demix88 <skrasnauskis@gmail.com>
2022-08-20 20:45:52 +0100

    bcrypt encryption

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 //jshint esversion:8
 require('dotenv').config()
 const express = require("express");
-const md5 = require("md5");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 const app=express();
 const ejs = require("ejs");
 const bodyParser=require("body-parser");
@@ -10,7 +12,7 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
 const mongoose = require("mongoose");
-console.log(md5("123456"));
+
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
 const userSchema = new mongoose.Schema({
@@ -31,35 +33,43 @@ app.get("/register", (req, res) => {
     res.render("register");
 });
 app.post("/register", (req, res) => {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
+
+    bcrypt.hash(req.body.password, saltRounds, (err, hash)=> {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save((err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("secrets");
+            }
+        });
+        });
     });
-    newUser.save((err) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("secrets");
-        }
-    });
-});
+
+
 
 app.post("/login", (req, res) => {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
     User.findOne({ email: username }, (err, foundUser) => {
         if (err) {
             console.log(err);
             
         } else {
             if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render("secrets");
+                bcrypt.compare(password, foundUser.password, function (err, result) {
+                    if (result == true) {
+                        res.render("secrets");
+ 
+                    }
+                });
                     
-                }
             }
         }
-    })
+    });
 });
 
 let port=process.env.PORT ;
